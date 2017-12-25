1098f67e9feb18cff0e5db742d45251e65d29cf7
baron517 <450175465@qq.com>
2017-12-25 22:50:58 +0800

    a

--- xiongshi_xin/js/visitorinfo.js
+++ xiongshi_xin/js/visitorinfo.js
@@ -59,6 +59,12 @@ var takeAddressList=[
 
 //台灣地區 1代表是 0代表否
 var IsTaiwan=1;
+if(IsTaiwan==0)
+{
+    $(".english-name1").attr("required","required");
+    $(".english-name2").attr("required","required");
+}
+
 
 //產品預購 1代表后台有勾选 0代表没有勾选
 var PurchaseBefore=1
@@ -215,27 +221,33 @@ function fenjiFunc()
 function nameVerify()
 {
 
+
+
     $(".traveller-col").each(function()
     {
 
-        if($(this).find(".zhongwen-name").val()==""&&($(this).find(".english-name1").val()==""||$(this).find(".english-name2").val()==""))
+        if(IsTaiwan==1)
         {
-            console.log($(this).find(".zhongwen-name"));
-            $(this).find(".zhongwen-name").addClass("form-error-input");
-
-            if( $(this).find(".zhongwen-name").parent().parent().find(".form-error-active").length>0)
+            if($(this).find(".zhongwen-name").val()==""&&($(this).find(".english-name1").val()==""||$(this).find(".english-name2").val()==""))
             {
-                $(this).find(".zhongwen-name").parent().parent().find(".form-error-active").text("英文姓名兩者擇一必填");
+
+                $(this).find(".zhongwen-name").addClass("form-error-input");
+                console.log($(this).find(".zhongwen-name").parent().parent().find(".form-error-active").length);
+
+                if( $(this).find(".zhongwen-name").parent().parent().find(".form-error-active").length>0)
+                {
+                    $(this).find(".zhongwen-name").parent().parent().find(".form-error-active").text("英文姓名兩者擇一必填");
+                }
+                else {
+                    $(this).find(".zhongwen-name").parent().parent().append('<label class="form-error-active">英文姓名兩者擇一必填</label>');
+                }
+
             }
             else {
-                $(this).find(".zhongwen-name").parent().parent().append('<label class="form-error-active">英文姓名兩者擇一必填</label>');
+
+                $(this).find(".zhongwen-name").removeClass("form-error-input");
+                $(this).find(".zhongwen-name").parent().next().removeClass("form-error-active");
             }
-
-        }
-        else {
-
-            $(this).find(".zhongwen-name").removeClass("form-error-input");
-            $(this).find(".zhongwen-name").parent().next().removeClass("form-error-active");
         }
 
     });
@@ -278,8 +290,9 @@ function verifyFunc()
     });
 
 
-    nameVerify();
     sameNameVerify();
+    nameVerify();
+
     youjiFunc();
     fenjiFunc();
 
@@ -299,8 +312,6 @@ function sameNameVerify()
 
         var isHasSameElementRs=isHasSameElement(zhongwenNameList);
 
-        console.log("重复元素:"+isHasSameElementRs);
-
         if(isHasSameElementRs!=-1&&$(this).find(".zhongwen-name").eq(isHasSameElementRs).val()!="")
         {
             $(this).find(".zhongwen-name").eq(isHasSameElementRs).addClass("form-error-input");
@@ -315,8 +326,8 @@ function sameNameVerify()
 
         }
         else{
-            $(this).find(".zhongwen-name").eq(isHasSameElementRs).removeClass("form-error-input");
-            $(this).find(".zhongwen-name").eq(isHasSameElementRs).parent().parent().find(".form-error-active").remove();
+            $(this).find(".zhongwen-name").removeClass("form-error-input");
+            $(this).find(".zhongwen-name").parent().parent().find(".form-error-active").remove();
         }
 
 
@@ -325,6 +336,8 @@ function sameNameVerify()
         {
             englishNameList.push($(this).val()+$(this).next().val());
         });
+
+        console.log(englishNameList);
 
         var isEnglishNameRs=isHasSameElement(englishNameList);
 
@@ -344,8 +357,9 @@ function sameNameVerify()
 
         }
         else{
-            $(this).find(".english-name1").eq(isEnglishNameRs).removeClass("form-error-input");
-            $(this).find(".english-name1").eq(isEnglishNameRs).parent().parent().find(".form-error-active").remove();
+            $(this).find(".english-name1").removeClass("form-error-input");
+            $(this).find(".english-name2").removeClass("form-error-input");
+            $(this).find(".english-name1").parent().parent().find(".form-error-active").remove();
         }
 
 
@@ -399,6 +413,12 @@ $(".calendar").on("input",function()
         {
             $(this).val(new Date().getFullYear()+$(this).val());
         }
+    }
+
+    if($(this).val().length>0)
+    {
+        $(this).removeClass("form-error");
+        $(this).parent().parent().find("label.form-error").remove();
     }
 
 
