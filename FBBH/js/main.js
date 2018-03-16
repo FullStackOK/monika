480a007ecd6dbcfa1bb9ca808e3aeba2548271ff
tuyao <tuyao@cbichina.com>
2018-03-16 09:34:22 +0800

    页面

--- FBBH/js/main.js
+++ FBBH/js/main.js
@@ -4,14 +4,6 @@ function main() {
 (function () {
    'use strict';
    
-   // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
 
    // Testimonial Slider
   	$('a.page-scroll').click(function() {
@@ -20,7 +12,7 @@ $(window).scroll(function() {
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
           if (target.length) {
             $('html,body').animate({
-              scrollTop: target.offset().top - 40
+              scrollTop: target.offset().top - 5
             }, 900);
             return false;
           }
@@ -31,59 +23,122 @@ $(window).scroll(function() {
     Show Menu on Book
     ======================================*/
     $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 100;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
+
+
+        videoHandle();
+
+
+
     });
 
-    $('body').scrollspy({ 
+    $(function()
+    {
+        videoHandle();
+    });
+
+
+    var prevScroll=0;
+    function videoHandle()
+    {
+
+
+        if(Math.abs(prevScroll-$(window).scrollTop())<=5)
+        {
+            return;
+        }
+
+        prevScroll=$(window).scrollTop();
+
+        var windowToTop=$(window).scrollTop();
+
+        var wHeight=$(window).height();
+
+        var aboutHeight=$("#about").offset().top+$("#about").height()-100;
+
+        var achivementsHeight=$("#achivements").offset().top+$("#achivements").height()-100;
+
+/*
+        console.log("windowToTop"+windowToTop);
+        console.log("wHeight"+wHeight);
+        console.log("aboutHeight"+aboutHeight);
+        console.log("achivementsHeight"+achivementsHeight);*/
+
+        if(windowToTop<=wHeight)
+        {
+
+
+            for(var i=0;i<$(".video-col").length;i++)
+            {
+                 if(!$(".video-col")[i].paused)
+                 {
+                     $(".video-col")[i].pause()
+                 }
+            }
+
+
+            if($(".video-col")[0].paused)
+            {
+                $(".video-col")[0].play();
+            }
+
+        }
+        else if(windowToTop>wHeight&&windowToTop<=aboutHeight)
+        {
+
+            for(var i=0;i<$(".video-col").length;i++)
+            {
+                if(!$(".video-col")[i].paused)
+                {
+                    $(".video-col")[i].pause()
+                }
+            }
+
+            if($(".video-col")[1].paused)
+            {
+                $(".video-col")[1].play();
+            }
+
+
+        }
+        else if(windowToTop>wHeight&&windowToTop<=achivementsHeight)
+        {
+
+            for(var i=0;i<$(".video-col").length;i++)
+            {
+                if(!$(".video-col")[i].paused)
+                {
+                    $(".video-col")[i].pause()
+                }
+            }
+
+            if($(".video-col")[2].paused)
+            {
+                $(".video-col")[2].play();
+            }
+
+
+        }
+        else
+        {
+            for(var i=0;i<$(".video-col").length;i++)
+            {
+                if(!$(".video-col")[i].paused)
+                {
+                    $(".video-col")[i].pause()
+                }
+            }
+        }
+
+
+
+    }
+
+
+    $('body').scrollspy({
         target: '.navbar-default',
         offset: 80
     })
 
-  	$(document).ready(function() {
-  	    $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
-
-  	});
-
-  	/*====================================
-    Portfolio Isotope Filter
-    ======================================*/
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
-    });
-	
 	// ****************************************************************
 // counterUp
 // ****************************************************************
@@ -97,12 +152,7 @@ $(window).scroll(function() {
 		}
 	});
 
-  	/*====================================
-    Pretty Photo
-    ======================================*/
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
+
 
 }());
 
